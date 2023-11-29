b9f7aaacc16fef8a4c49ea677083b05819e1968a
hrjuno <harjunoabdullah@gmail.com>
2023-11-29 16:43:02 +0700

    membuat screens modul pinjam buku

---
+++ lib/pinjam_buku/screens/pinjam_buku_form.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:pbp_django_auth/pbp_django_auth.dart';
+
+class BorrowFormPage extends StatefulWidget {
+  const BorrowFormPage({super.key});
+
+  @override
+  State<BorrowFormPage> createState() => _BorrowFormPageState();
+}
+
+class _BorrowFormPageState extends State<BorrowFormPage>{
+  final _formKey = GlobalKey<FormState>();
+  DateTime _returnDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    final request = context.watch<CookieRequest>();
+
+    return Scaffold(
+
+    )
+  }
+
+}
