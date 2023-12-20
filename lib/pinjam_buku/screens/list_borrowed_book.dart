035551a5bb5f7fcee7024eb4bf1c9fe17c8e032b
hrjuno <harjunoabdullah@gmail.com>
2023-12-20 22:56:22 +0700

    edit halaman form pinjam buku

--- lib/pinjam_buku/screens/list_borrowed_book.dart
+++ lib/pinjam_buku/screens/list_borrowed_book.dart
@@ -17,6 +17,9 @@ class BorrowedBookPage extends StatefulWidget {
 
 class _BorrowedBookPageState extends State<BorrowedBookPage> {
   List<Book> listBorrowedBook = [];
+  List<Book> filteredList = [];
+  bool isSearching = false;
+
   Future<List<Book>> fetchWishlist(CookieRequest cookieRequest) async {
     var response = await cookieRequest
         .get("https://readnow-c14-tk.pbp.cs.ui.ac.id/pinjam/get-borrowed-book/");
@@ -43,7 +46,6 @@ class _BorrowedBookPageState extends State<BorrowedBookPage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        automaticallyImplyLeading: false,
         toolbarHeight: height * 0.1 <= 150 ? height * 0.1 : 150,
         scrolledUnderElevation: 5,
         surfaceTintColor: Colors.transparent,
@@ -51,6 +53,7 @@ class _BorrowedBookPageState extends State<BorrowedBookPage> {
         elevation: 5,
         title: Stack(
           children: [
+            // Wishlist
             SizedBox(
               width: width,
               child: Text(
@@ -77,77 +80,183 @@ class _BorrowedBookPageState extends State<BorrowedBookPage> {
             ),
           ],
         ),
-
         backgroundColor: Colors.white,
         foregroundColor: Colors.black,
       ),
-      body: FutureBuilder(
-          future: fetchWishlist(request),
-          builder: (context, AsyncSnapshot snapshot) {
-            if (snapshot.data == null) {
-              return const Center(child: CircularProgressIndicator());
-            } else {
-              if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.book_outlined,
-                        size: width * 0.3 <= 180 ? width * 0.3 : 180,
-                        color: Colors.grey[500],
-                      ),
-                      Text(
-                        "You haven't borrowed any books",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: width * 0.1 <= 20 ? width * 0.1 : 20,
-                          fontWeight: FontWeight.w500,
-                          fontStyle: FontStyle.italic,
-                          color: Colors.grey[500],
-                        ),
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.all(8.0),
-                      ),
-                      SizedBox(
-                        width: width * 0.6,
-                        child: Text(
-                          "Nothing to show",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: width * 0.1 <= 13 ? width * 0.1 : 13,
-                            color: Colors.grey[500],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              } else {
-                return ListView.builder(
-                    padding: const EdgeInsets.only(top: 0),
-                    shrinkWrap: true, // Menambahkan ini
-                    itemCount: snapshot.data!.length,
-                    itemBuilder: (_, index) => InkWell(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => BookDetails(
-                                book: snapshot.data![index],
+      body: Column(
+        children: [
+
+          Padding(
+            padding: EdgeInsets.fromLTRB(
+                width * 0.05, height * 0.02, width * 0.05, height * 0.02),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search by Title, Author, or ISBN',
+                hintStyle: TextStyle(
+                  fontSize: width * 0.1 <= 15 ? width * 0.1 : 15,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey[500],
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: Colors.grey[500],
+                ),
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide.none,
+                ),
+              ),
+              onChanged: (value) {
+                if (value.isNotEmpty) {
+                  setState(() {
+                    isSearching = true;
+                    filteredList = filterItem(value);
+                  });
+                } else {
+                  setState(() {
+                    isSearching = false;
+                  });
+                }
+              },
+            ),
+          ),
+
+          Expanded(
+            child: FutureBuilder(
+                future: fetchWishlist(request),
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (snapshot.data == null) {
+                    return const Center(child: CircularProgressIndicator());
+                  } else {
+                    if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                      return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.book_outlined,
+                              size: width * 0.3 <= 180 ? width * 0.3 : 180,
+                              color: Colors.grey[500],
+                            ),
+                            Text(
+                              "You haven't borrowed any books",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: width * 0.1 <= 20 ? width * 0.1 : 20,
+                                fontWeight: FontWeight.w500,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.grey[500],
                               ),
                             ),
+                            const Padding(
+                              padding: EdgeInsets.all(8.0),
+                            ),
+                            SizedBox(
+                              width: width * 0.6,
+                              child: Text(
+                                "Nothing to show",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize:
+                                  width * 0.1 <= 13 ? width * 0.1 : 13,
+                                  color: Colors.grey[500],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    } else {
+                      if (isSearching) {
+                        if (filteredList.isEmpty) {
+                          return Center(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Icon(
+                                  Icons.book_outlined,
+                                  size: width * 0.3 <= 180 ? width * 0.3 : 180,
+                                  color: Colors.grey[500],
+                                ),
+                                Text(
+                                  "Book is Not Found",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize:
+                                    width * 0.1 <= 20 ? width * 0.1 : 20,
+                                    fontWeight: FontWeight.w500,
+                                    fontStyle: FontStyle.italic,
+                                    color: Colors.grey[500],
+                                  ),
+                                ),
+                              ],
+                            ),
                           );
-                        },
-                        child: BorrowedCard(
-                            book: snapshot.data![index],
-                            onTap: () => removeItem(
-                                snapshot.data![index].pk, request))));
-              }
-            }
-          }),
+                        }
+                        return ListView.builder(
+                            padding: const EdgeInsets.only(top: 0),
+                            shrinkWrap: true,
+                            itemCount: filteredList.length,
+                            itemBuilder: (_, index) => InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => BookDetails(
+                                        book: filteredList[index],
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: BorrowedCard(
+                                    book: filteredList[index],
+                                    onTap: () => removeItem(
+                                        filteredList[index].pk, request))));
+                      } else {
+                        return ListView.builder(
+                            padding: const EdgeInsets.only(top: 0),
+                            shrinkWrap: true,
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (_, index) => InkWell(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => BookDetails(
+                                        book: snapshot.data![index],
+                                      ),
+                                    ),
+                                  );
+                                },
+                                child: BorrowedCard(
+                                    book: snapshot.data![index],
+                                    onTap: () => removeItem(
+                                        snapshot.data![index].pk, request))));
+                      }
+                    }
+                  }
+                }),
+          ),
+        ],
+      ),
     );
+  }
+
+  List<Book> filterItem(String query) {
+    List<Book> filteredList = [];
+    if (kDebugMode) {
+      print("the query is: $query");
+    }
+    for (var item in listBorrowedBook) {
+      if (item.fields.title.toLowerCase().contains(query.toLowerCase()) ||
+          item.fields.authors.toLowerCase().contains(query.toLowerCase()) ||
+          item.fields.isbn.toLowerCase().contains(query.toLowerCase())) {
+        filteredList.add(item);
+      }
+    }
+    return filteredList;
   }
 
   removeItem(int id, CookieRequest request) async {
